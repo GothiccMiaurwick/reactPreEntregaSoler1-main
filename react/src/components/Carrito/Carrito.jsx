87f79d7240f65292613b87fbdc47a4b7fc5e367d
Miaurwick <samwich405@gmail.com>
2024-02-15 19:31:13 -0500

    Estilos carrito actualizados

--- react/src/components/Carrito/Carrito.jsx
+++ react/src/components/Carrito/Carrito.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
 import { CartContext } from "../context/CartContext";
 import { Link } from "react-router-dom";
+import "./Carrito.css"
 
 const Carrito = () => {
   const { carrito, precioTotal, borrarProducto, borrarTodo } =
@@ -15,28 +16,28 @@ const Carrito = () => {
     );
   }
 
-  return (
-    <div>
-      <ul>
-        {carrito.map((producto) => (
-          <li key={producto.id}>
-            <img src={producto.imagen} alt="" />
-            <p>{producto.nombre}</p>
-            <p>Cantidad: {producto.cantidad}</p>
-            <p>Precio Unitario: {producto.precio}</p>
-            <button onClick={() => borrarProducto(producto.id)}>
-              Eliminar
-            </button>
-          </li>
-        ))}
-      </ul>
-      <div>
-        <p> precio total: {precioTotal()}</p>
-        <button onClick={() => borrarTodo()}>Vaciar carrito</button>
-        <Link to="/checkout">Continuar Compra</Link>
+    return (
+      <div className="carrito">
+        <ul>
+          {carrito.map((producto) => (
+            <li key={producto.id}>
+              <img src={producto.imagen} alt="" />
+              <p>{producto.nombre}</p>
+              <p>Cantidad: {producto.cantidad}</p>
+              <p>Precio Unitario: {producto.precio}</p>
+              <button onClick={() => borrarProducto(producto.id)}>
+                Eliminar
+              </button>
+            </li>
+          ))}
+        </ul>
+        <div>
+          <p> precio total: {precioTotal()}</p>
+          <button onClick={() => borrarTodo()}>Vaciar carrito</button>
+          <Link to="/checkout">Continuar Compra</Link>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  };
 
 export default Carrito;
